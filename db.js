66542592179b0523bd10bb4aff5ac9b05a4251a2
Yakov Kondratev <kondratev@nerve.ai>
2024-02-09 18:12:04 +0300

    vercel

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 const Pool=  require('pg').Pool
+// const pool = new Pool({
+//     user: 'postgres',
+//     password: 'newpassword',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'node_postgres'
+// })
 const pool = new Pool({
-    user: 'postgres',
-    password: 'newpassword',
-    host: 'localhost',
-    port: 5432,
-    database: 'node_postgres'
+    connectionString: process.env.POSTGRES_URL ,
 })
 
 module.exports = pool
